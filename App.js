13ec7df9245fd192056e5db06fb75ec6994a5e9a
Russell Taylor <rdtaylorjr@protonmail.com>
2021-07-06 11:21:51 -0600

    Add Navigation to Register and Verify Screens

--- App.js
+++ App.js
@@ -1,17 +1,70 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, {Component} from 'react'
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native'
 import Registration from './Registration'
 import Verification from './Verification'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
 
-class App extends React.Component {
+class HomeScreen extends React.Component {
    render() {
        return (
-       <View>
-           {/* <Registration /> */}
-           <Verification />
+       <View style={{flex: 1, alignItems:"center"}}>
+          <Text style= {{ fontSize: 30, color: "#000", marginBottom: 50, marginTop: 100 }}>
+              Paceport Face-Rekon
+          </Text>
+          <TouchableHighlight style={[styles.buttonContainer, styles.button]} onPress={() => this.props.navigation.navigate('Registration')}>
+              <Text style={styles.buttonText}>
+                    RegisterFace/AdminOnly
+              </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={[styles.buttonContainer, styles.button]} onPress={() => this.props.navigation.navigate('Verification')}>
+              <Text style={styles.buttonText}>
+                    Payment
+              </Text>
+          </TouchableHighlight>
        </View>
        );
    }
 }
 
-export default App;
+const MainNavigator = createStackNavigator(
+    {
+        Home: {screen: HomeScreen},
+        Registration: {screen: Registration},
+        Verification: {screen: Verification}
+    },
+    {
+        initialRouteName: 'Home',
+    }
+ );
+
+ const AppContainer = createAppContainer(MainNavigator);
+ 
+ export default class App extends Component {
+    render() {
+        return <AppContainer />;
+    }
+ }
+
+ const styles = StyleSheet.create({
+    buttonContainer: {
+        height:45,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom:20,
+        width:"80%",
+        borderRadius:30,
+        marginTop: 20,
+        marginLeft: 5,
+    },
+    button: {
+        backgroundColor: "#337ab7",
+        width: 350,
+        marginTop: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+ })
