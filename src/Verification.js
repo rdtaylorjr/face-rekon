a1640b5b981660aa1ea548cb4df262f1cb01ec3b
Russell Taylor <rdtaylorjr@protonmail.com>
2021-07-13 07:49:26 -0600

    Add Android Liveness Detection

--- src/Verification.js
+++ src/Verification.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useReducer, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import * as FaceDetector from 'expo-face-detector'
 import { Alert, Dimensions, StyleSheet, Text, View } from 'react-native'
 import { Camera } from 'expo-camera'
 import { AnimatedCircularProgress } from 'react-native-circular-progress'
-import { useNavigation } from '@react-navigation/native'
-import Svg, { Path } from 'react-native-svg'
+import MaskedView from '@react-native-community/masked-view'
 import Amplify, { API } from 'aws-amplify'
 
 Amplify.configure({
@@ -20,17 +19,26 @@ Amplify.configure({
 
 const { width: windowWidth } = Dimensions.get('window')
 
-const detections = {
-  BLINK: { promptText: 'Blink both eyes', minProbability: 0.4 },
-  TURN_HEAD_LEFT: { promptText: 'Turn head left', maxAngle: -7.5 },
-  TURN_HEAD_RIGHT: { promptText: 'Turn head right', minAngle: 7.5 },
-  NOD: { promptText: 'Nod', minDiff: 1 },
-  SMILE: { promptText: 'Smile', minProbability: 0.7 }
+const PREVIEW_SIZE = 325
+const PREVIEW_RECT = {
+  minX: (windowWidth - PREVIEW_SIZE) / 2,
+  minY: 50,
+  width: PREVIEW_SIZE,
+  height: PREVIEW_SIZE
 }
 
-const promptsText = {
-  noFaceDetected: 'No face detected',
-  performActions: 'Perform the following actions:'
+const instructionsText = {
+  initialPrompt: 'Position your face in the circle',
+  performActions: 'Keep the device still and perform\nthe following actions:',
+  tooClose: 'You\'re too close. Hold the device further.'
+}
+
+const detections = {
+  BLINK: { instruction: 'Blink both eyes', minProbability: 0.3 },
+  TURN_HEAD_LEFT: { instruction: 'Turn head left', maxAngle: -15 },
+  TURN_HEAD_RIGHT: { instruction: 'Turn head right', minAngle: 15 },
+  NOD: { instruction: 'Nod', minDiff: 1.5 },
+  SMILE: { instruction: 'Smile', minProbability: 0.7 }
 }
 
 const detectionsList = [
@@ -42,22 +50,57 @@ const detectionsList = [
 ]
 
 const initialState = {
-  faceDetected: false,
-  promptText: promptsText.noFaceDetected,
+  faceDetected: 'no',
+  faceTooBig: 'no',
   detectionsList,
   currentDetectionIndex: 0,
   progressFill: 0,
   processComplete: false
 }
 
-export default function Detection() {
-  const navigation = useNavigation()
-  const [hasPermission, setHasPermission] = useState(false)
-  const [state, dispatch] = useReducer(detectionReducer, initialState)
-  const rollAngles = useRef()
-  const rect = useRef()
+const detectionReducer = (state, action) => {
+  switch (action.type) {
+    case 'FACE_DETECTED':
+      if (action.payload === 'yes') {
+        return {
+          ...state,
+          faceDetected: action.payload,
+          progressFill: 100 / (state.detectionsList.length + 1)
+        }
+      } 
+      else {
+        return initialState
+      }
 
-  useEffect(() => {
+    case 'FACE_TOO_BIG':
+      return { ...state, faceTooBig: action.payload }
+
+    case 'NEXT_DETECTION':
+      const nextDetectionIndex = state.currentDetectionIndex + 1
+
+      const progressMultiplier = nextDetectionIndex + 1
+
+      const newProgressFill =
+        (100 / (state.detectionsList.length + 1)) * progressMultiplier
+
+      if (nextDetectionIndex === state.detectionsList.length)
+        return { ...state, processComplete: true, progressFill: newProgressFill }
+      else
+        return { ...state, currentDetectionIndex: nextDetectionIndex, progressFill: newProgressFill }
+
+    default:
+      throw new Error('Unexpected action type.')
+  }
+}
+
+export default function Verification({ navigation, route }) {
+
+  const [hasPermission, setHasPermission] = React.useState(false)
+  const [state, dispatch] = React.useReducer(detectionReducer, initialState)
+  const rollAngles = React.useRef([])
+  const [cameraRef, setCameraRef] = useState(null)
+
+  React.useEffect(() => {
     const requestPermissions = async () => {
       const { status } = await Camera.requestPermissionsAsync()
       setHasPermission(status === 'granted')
@@ -65,42 +108,65 @@ export default function Detection() {
     requestPermissions()
   }, [])
 
-  const drawFaceRect = (face) => {
-    rect.current?.setNativeProps({
-      width: face.bounds.size.width,
-      height: face.bounds.size.height,
-      top: face.bounds.origin.y,
-      left: face.bounds.origin.x
-    })
-  }
-
   const onFacesDetected = (result) => {
     if (result.faces.length !== 1) {
-      dispatch({ type: 'FACE_DETECTED', value: 'no' })
+      dispatch({ type: 'FACE_DETECTED', payload: 'no' })
       return
     }
 
     const face = result.faces[0]
-    const midFaceOffsetY = face.bounds.size.height / 2
-    const midFaceOffsetX = face.bounds.size.width / 2
+    const faceRect = {
+      minX: face.bounds.origin.x,
+      minY: face.bounds.origin.y,
+      width: face.bounds.size.width,
+      height: face.bounds.size.height
+    }
 
-    drawFaceRect(face)
-    const faceMidYPoint = face.bounds.origin.y + midFaceOffsetY
-    console.log(`face.bounds.origin.y: ${face.bounds.origin.y}`)
+    const edgeOffset = 50
+    const faceRectSmaller = {
+      width: faceRect.width - edgeOffset,
+      height: faceRect.height - edgeOffset,
+      minY: faceRect.minY + edgeOffset / 2,
+      minX: faceRect.minX + edgeOffset / 2
+    }
 
-    if (faceMidYPoint <= PREVIEW_MARGIN_TOP || faceMidYPoint >= PREVIEW_SIZE + PREVIEW_MARGIN_TOP) {
-      dispatch({ type: 'FACE_DETECTED', value: 'no' })
+    const contains = ({ outside, inside }) => {
+      const outsideMaxX = outside.minX + outside.width
+      const insideMaxX = inside.minX + inside.width
+  
+      const outsideMaxY = outside.minY + outside.height
+      const insideMaxY = inside.minY + inside.height
+  
+      if (inside.minX < outside.minX || insideMaxX > outsideMaxX || inside.minY < outside.minY || insideMaxY > outsideMaxY)
+          return false
+      else
+          return true
+    }
+
+    const previewContainsFace = contains({
+      outside: PREVIEW_RECT,
+      inside: faceRectSmaller
+    })
+
+    if (!previewContainsFace) {
+      dispatch({ type: 'FACE_DETECTED', payload: 'no' })
       return
     }
 
-    const faceMidXPoint = face.bounds.origin.x + midFaceOffsetX
-    if (faceMidXPoint <= windowWidth / 2 - PREVIEW_SIZE / 2 || faceMidXPoint >= windowWidth / 2 + PREVIEW_SIZE / 2) {
-      dispatch({ type: 'FACE_DETECTED', value: 'no' })
-      return
+    if (state.faceDetected === 'no') {
+      const faceMaxSize = PREVIEW_SIZE - 90
+      if (faceRect.width >= faceMaxSize && faceRect.height >= faceMaxSize) {
+        dispatch({ type: 'FACE_TOO_BIG', payload: 'yes' })
+        return
+      }
+
+      if (state.faceTooBig === 'yes') {
+        dispatch({ type: 'FACE_TOO_BIG', payload: 'no' })
+      }
     }
 
-    if (!state.faceDetected) {
-      dispatch({ type: 'FACE_DETECTED', value: 'yes' })
+    if (state.faceDetected === "no") {
+      dispatch({ type: 'FACE_DETECTED', payload: 'yes' })
     }
 
     const detectionAction = state.detectionsList[state.currentDetectionIndex]
@@ -111,48 +177,46 @@ export default function Detection() {
           face.leftEyeOpenProbability <= detections.BLINK.minProbability
         const rightEyeClosed =
           face.rightEyeOpenProbability <= detections.BLINK.minProbability
-        if (leftEyeClosed && rightEyeClosed) {
-          dispatch({ type: 'NEXT_DETECTION', value: null })
-        }
+        if (leftEyeClosed && rightEyeClosed)
+          dispatch({ type: 'NEXT_DETECTION', payload: null })
         return
+
       case 'NOD':
         rollAngles.current.push(face.rollAngle)
 
-        if (rollAngles.current.length > 10) {
+        if (rollAngles.current.length > 10)
           rollAngles.current.shift()
-        }
 
-        if (rollAngles.current.length < 10) return
+        if (rollAngles.current.length < 10) 
+          return
 
-        const rollAnglesExceptCurrent = [...rollAngles.current].splice(
-          0,
-          rollAngles.current.length - 1
-        )
+        const rollAnglesExceptCurrent = [...rollAngles.current].splice(0, rollAngles.current.length - 1)
+
         const rollAnglesSum = rollAnglesExceptCurrent.reduce((prev, curr) => {
           return prev + Math.abs(curr)
         }, 0)
+
         const avgAngle = rollAnglesSum / rollAnglesExceptCurrent.length
-        
+
         const diff = Math.abs(avgAngle - Math.abs(face.rollAngle))
 
-        if (diff >= detections.NOD.minDiff) {
-          dispatch({ type: 'NEXT_DETECTION', value: null })
-        }
+        if (diff >= detections.NOD.minDiff)
+          dispatch({ type: 'NEXT_DETECTION', payload: null })
         return
+
       case 'TURN_HEAD_LEFT':
-        if (face.yawAngle <= detections.TURN_HEAD_LEFT.maxAngle) {
-          dispatch({ type: 'NEXT_DETECTION', value: null })
-        }
+        if (face.yawAngle <= detections.TURN_HEAD_LEFT.maxAngle)
+          dispatch({ type: 'NEXT_DETECTION', payload: null })
         return
+
       case 'TURN_HEAD_RIGHT':
-        if (face.yawAngle >= detections.TURN_HEAD_RIGHT.minAngle) {
-          dispatch({ type: 'NEXT_DETECTION', value: null })
-        }
+        if (face.yawAngle >= detections.TURN_HEAD_RIGHT.minAngle)
+          dispatch({ type: 'NEXT_DETECTION', payload: null })
         return
+        
       case 'SMILE':
-        if (face.smilingProbability >= detections.SMILE.minProbability) {
-          dispatch({ type: 'NEXT_DETECTION', value: null })
-        }
+        if (face.smilingProbability >= detections.SMILE.minProbability)
+          dispatch({ type: 'NEXT_DETECTION', payload: null })
         return
     }
   }
@@ -208,7 +272,7 @@ export default function Detection() {
     }
   }
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (state.processComplete) {
       captureImage()
     }
@@ -220,159 +284,109 @@ export default function Detection() {
 
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          position: "absolute",
-          top: 0,
-          width: "100%",
-          height: PREVIEW_MARGIN_TOP,
-          backgroundColor: "white",
-          zIndex: 10
-        }}
-      />
-      <View
-        style={{
-          position: "absolute",
-          top: PREVIEW_MARGIN_TOP,
-          left: 0,
-          width: (windowWidth - PREVIEW_SIZE) / 2,
-          height: PREVIEW_SIZE,
-          backgroundColor: "white",
-          zIndex: 10
-        }}
-      />
-      <View
-        style={{
-          position: "absolute",
-          top: PREVIEW_MARGIN_TOP,
-          right: 0,
-          width: (windowWidth - PREVIEW_SIZE) / 2 + 1,
-          height: PREVIEW_SIZE,
-          backgroundColor: "white",
-          zIndex: 10
-        }}
-      />
-
-      <Camera
-        style={styles.cameraPreview}
-        type={Camera.Constants.Type.front}
-        onFacesDetected={onFacesDetected}
-        faceDetectorSettings={{
-          mode: FaceDetector.Constants.Mode.fast,
-          detectLandmarks: FaceDetector.Constants.Landmarks.none,
-          runClassifications: FaceDetector.Constants.Classifications.all,
-          minDetectionInterval: 0,
-          tracking: false
-        }}
+      <MaskedView
+        style={StyleSheet.absoluteFill}
+        maskElement={<View style={styles.mask} />}
       >
-        <CameraPreviewMask width={"100%"} style={styles.circularProgress} />
-        <AnimatedCircularProgress
-          style={styles.circularProgress}
-          size={PREVIEW_SIZE}
-          width={5}
-          backgroundWidth={7}
-          fill={state.progressFill}
-          tintColor="#3485FF"
-          backgroundColor="#e8e8e8"
-        />
-      </Camera>
-      <View
-        ref={rect}
-        style={{
-          position: "absolute",
-          borderWidth: 2,
-          borderColor: "pink",
-          zIndex: 10
-        }}
-      />
-      <View style={styles.promptContainer}>
-        <Text style={styles.faceStatus}>
-          {!state.faceDetected && promptsText.noFaceDetected}
-        </Text>
-        <Text style={styles.actionPrompt}>
-          {state.faceDetected && promptsText.performActions}
-        </Text>
-        <Text style={styles.action}>
-          {state.faceDetected &&
-            detections[state.detectionsList[state.currentDetectionIndex]]
-              .promptText}
-        </Text>
+        <Camera
+          style={StyleSheet.absoluteFill}
+          type={Camera.Constants.Type.front}
+          onFacesDetected={onFacesDetected}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.none,
+            runClassifications: FaceDetector.Constants.Classifications.all,
+            minDetectionInterval: 125,
+            tracking: false
+          }}
+          ref={ref => {setCameraRef(ref)}}
+        >
+          <AnimatedCircularProgress
+            style={styles.circularProgress}
+            size={PREVIEW_SIZE}
+            width={5}
+            backgroundWidth={7}
+            fill={state.progressFill}
+            tintColor="#009688"
+            backgroundColor="#e8e8e8"
+          />
+        </Camera>
+      </MaskedView>
+      <View style={styles.instructionsContainer}>
+        {!state.processComplete &&
+          <Text style={styles.instructions}>
+            {state.faceDetected === "no" &&
+              state.faceTooBig === "no" &&
+              instructionsText.initialPrompt}
+
+            {state.faceTooBig === "yes" && 
+              instructionsText.tooClose}
+
+            {state.faceDetected === "yes" &&
+              state.faceTooBig === "no" &&
+              instructionsText.performActions}
+          </Text>
+        }
+        {!state.processComplete &&
+          <Text style={styles.action}>
+            {state.faceDetected === "yes" &&
+              state.faceTooBig === "no" &&
+              detections[state.detectionsList[state.currentDetectionIndex]]
+                .instruction}
+          </Text>
+        }
+        {state.processComplete &&
+          <Text style={styles.processing}>
+            Processing payment...
+          </Text>
+        }
       </View>
     </View>
   )
 }
 
-const detectionReducer = (state, action) => {
-  const numDetections = state.detectionsList.length
-  const newProgressFill = (100 / (numDetections + 1)) * (state.currentDetectionIndex + 1)
-
-  switch (action.type) {
-    case 'FACE_DETECTED':
-      if (action.value === 'yes')
-        return { ...state, faceDetected: true, progressFill: newProgressFill }
-      else
-        return initialState
-    case 'NEXT_DETECTION':
-      const nextIndex = state.currentDetectionIndex + 1
-      if (nextIndex === numDetections)
-        return { ...state, processComplete: true, progressFill: 100 }
-      else
-        return { ...state, currentDetectionIndex: nextIndex, progressFill: newProgressFill }
-    default:
-      throw new Error('Unexpeceted action type.')
-  }
-}
-
-const CameraPreviewMask = (props) => (
-  <Svg width={300} height={300} viewBox="0 0 300 300" fill="none" {...props}>
-    <Path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M150 0H0v300h300V0H150zm0 0c82.843 0 150 67.157 150 150s-67.157 150-150 150S0 232.843 0 150 67.157 0 150 0z"
-      fill="#fff"
-    />
-  </Svg>
-)
-
-const PREVIEW_MARGIN_TOP = 50
-const PREVIEW_SIZE = 300
-
 const styles = StyleSheet.create({
-  actionPrompt: {
-    fontSize: 20,
-    textAlign: "center"
-  },
   container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  promptContainer: {
-    position: "absolute",
-    alignSelf: "center",
-    top: PREVIEW_MARGIN_TOP + PREVIEW_SIZE,
-    height: "100%",
-    width: "100%",
-    backgroundColor: "white"
-  },
-  faceStatus: {
-    fontSize: 24,
-    textAlign: "center",
-    marginTop: 10
-  },
-  cameraPreview: {
     flex: 1
   },
+  mask: {
+    borderRadius: PREVIEW_SIZE / 2,
+    height: PREVIEW_SIZE,
+    width: PREVIEW_SIZE,
+    marginTop: PREVIEW_RECT.minY,
+    alignSelf: "center",
+    backgroundColor: "white"
+  },
   circularProgress: {
-    position: "absolute",
     width: PREVIEW_SIZE,
     height: PREVIEW_SIZE,
-    top: PREVIEW_MARGIN_TOP,
-    alignSelf: "center"
+    marginTop: PREVIEW_RECT.minY,
+    marginLeft: PREVIEW_RECT.minX
+  },
+  instructionsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: PREVIEW_RECT.minY + PREVIEW_SIZE
+  },
+  instructions: {
+    fontSize: 20,
+    textAlign: "center",
+    position: "absolute",
+    top: 15
   },
   action: {
     fontSize: 24,
     textAlign: "center",
-    marginTop: 10,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    position: "absolute",
+    top: 70
+  },
+  processing: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+    position: "absolute",
+    top: 25
   }
 })
