594f0aee1b5814a68f05650471d09f6a240f9641
Russell Taylor <rdtaylorjr@protonmail.com>
2021-07-22 21:32:55 -0600

    Update Registration.js

--- Registration.js
+++ Registration.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { StyleSheet, View, Text, TextInput, Image, TouchableHighlight, Alert } from 'react-native'
-import * as ImagePicker from 'expo-image-picker'
+import React, { useState } from 'react'
+import * as FaceDetector from 'expo-face-detector'
+import { Camera } from 'expo-camera'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
+import MaskedView from '@react-native-community/masked-view'
 import Amplify, { API } from 'aws-amplify'
-import logo from '../assets/icon.png'
 
 Amplify.configure({
   API: {
@@ -15,200 +17,267 @@ Amplify.configure({
   }
 })
 
-export default class Registration extends React.Component {
+const { width: windowWidth } = Dimensions.get('window')
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: '',
-      capturedImage: ''
-    }
-  }
+const PREVIEW_SIZE = 325
+const PREVIEW_RECT = {
+  minX: (windowWidth - PREVIEW_SIZE) / 2,
+  minY: 50,
+  width: PREVIEW_SIZE,
+  height: PREVIEW_SIZE
+}
 
-  captureImageButtonHandler = async () => {
-    await ImagePicker.requestCameraPermissionsAsync()
+const instructionsText = {
+  initialPrompt: 'Position your face in the circle',
+  tooClose: 'You\'re too close. Hold the device further.'
+}
 
-    let result = await ImagePicker.launchCameraAsync({
-      maxWidth: 800,
-      maxHeight: 600,
-      base64: true
-    })
+const initialState = {
+  faceDetected: 'no',
+  faceTooBig: 'no',
+  progressFill: 0,
+  processComplete: false
+}
 
-    if (!result.cancelled) {
-      this.setState({ capturedImage: result.uri, base64String: result.base64 })
-    }
-
-  }
-
-  registerButtonHandler = () => {
-    if (this.state.username == '' || this.state.username == undefined || this.state.username == null) {
-      alert('Please Enter the Username')
-    }
-    else if (this.state.capturedImage == '' || this.state.capturedImage == undefined || this.state.capturedImage == null) {
-      alert('Please Capture the Image')
-    }
-    else {
-      const apiName = 'face-payment'
-      const path = '/register'
-
-      const init = {
-        headers: {
-          "Accept": "application/json",
-          "X-Amz-Target": "RekognitionService.IndexFaces",
-          "Content-Type": "application/x-amz-json-1.1"
-        },
-        body: JSON.stringify({
-          name: this.state.username,
-          Image: this.state.base64String
-        })
+const detectionReducer = (state, action) => {
+  switch (action.type) {
+    case 'FACE_DETECTED':
+      if (action.payload === 'yes') {
+        return { ...state, processComplete: true }
+      }
+      else {
+        return initialState
       }
 
-      API.post(apiName, path, init).then(response => {
-        if (response.errorMessage) {
-          throw response.errorMessage
-        }
-        Alert.alert('Registration Successful', JSON.stringify(response))
-      }).catch(error => {
-        Alert.alert('Error', error)
+    case 'FACE_TOO_BIG':
+      return { ...state, faceTooBig: action.payload }
+
+    default:
+      throw new Error('Unexpected action type.')
+  }
+}
+
+export default function Registration({ navigation, route }) {
+
+  const [hasPermission, setHasPermission] = React.useState(false)
+  const [state, dispatch] = React.useReducer(detectionReducer, initialState)
+  const rollAngles = React.useRef([])
+  const [cameraRef, setCameraRef] = useState(null)
+
+  React.useEffect(() => {
+    const requestPermissions = async () => {
+      const { status } = await Camera.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    }
+    requestPermissions()
+  }, [])
+
+  const onFacesDetected = (result) => {
+    if (result.faces.length !== 1) {
+      dispatch({ type: 'FACE_DETECTED', payload: 'no' })
+      return
+    }
+
+    const face = result.faces[0]
+    const faceRect = {
+      minX: face.bounds.origin.x,
+      minY: face.bounds.origin.y,
+      width: face.bounds.size.width,
+      height: face.bounds.size.height
+    }
+
+    const edgeOffset = 50
+    const faceRectSmaller = {
+      width: faceRect.width - edgeOffset,
+      height: faceRect.height - edgeOffset,
+      minY: faceRect.minY + edgeOffset / 2,
+      minX: faceRect.minX + edgeOffset / 2
+    }
+
+    const contains = ({ outside, inside }) => {
+      const outsideMaxX = outside.minX + outside.width
+      const insideMaxX = inside.minX + inside.width
+
+      const outsideMaxY = outside.minY + outside.height
+      const insideMaxY = inside.minY + inside.height
+
+      if (inside.minX < outside.minX || insideMaxX > outsideMaxX || inside.minY < outside.minY || insideMaxY > outsideMaxY)
+        return false
+      else
+        return true
+    }
+
+    const previewContainsFace = contains({
+      outside: PREVIEW_RECT,
+      inside: faceRectSmaller
+    })
+
+    if (!previewContainsFace) {
+      dispatch({ type: 'FACE_DETECTED', payload: 'no' })
+      return
+    }
+
+    if (state.faceDetected === 'no') {
+      const faceMaxSize = PREVIEW_SIZE - 90
+      if (faceRect.width >= faceMaxSize && faceRect.height >= faceMaxSize) {
+        dispatch({ type: 'FACE_TOO_BIG', payload: 'yes' })
+        return
+      }
+
+      if (state.faceTooBig === 'yes') {
+        dispatch({ type: 'FACE_TOO_BIG', payload: 'no' })
+      }
+    }
+
+    if (state.faceDetected === "no") {
+      dispatch({ type: 'FACE_DETECTED', payload: 'yes' })
+    }
+  }
+
+  const registerFace = (photo) => {
+    const apiName = 'face-payment'
+    const path = '/register'
+
+    const init = {
+      headers: {
+        "Accept": "application/json",
+        "X-Amz-Target": "RekognitionService.IndexFaces",
+        "Content-Type": "application/x-amz-json-1.1"
+      },
+      body: JSON.stringify({
+        name: route.params.username,
+        Image: photo.base64
+      })
+    }
+
+    API.post(apiName, path, init).then(response => {
+      if (response.errorMessage) {
+        throw response.errorMessage
+      }
+      else {
+        const parsed = JSON.parse(response.body)
+        navigation.navigate('Confirmation', {
+          username: route.params.username,
+          type: 'registration'
+        })
+      }
+    }).catch(error => {
+      Alert.alert('Error', error)
+      navigation.navigate('Home')
+    })
+  }
+
+  const captureImage = async () => {
+    if (cameraRef) {
+      let photo = await cameraRef.takePictureAsync({ base64: true })
+      registerFace(photo)
+    }
+    else {
+      navigation.navigate('Registration', {
+        paymentAmount: route.params.paymentAmount
       })
     }
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image source={logo} style={styles.logo}/>
-        <Text style={styles.title}>Register Face</Text>
+  React.useEffect(() => {
+    if (state.processComplete) {
+      captureImage()
+    }
+  }, [state.processComplete])
 
-        <TextInput
-          placeholder="Enter Username"
-          onChangeText={UserName => this.setState({ username: UserName })}
-          underlineColorAndroid='transparent'
-          style={styles.input}
-        />
-
-        {this.state.capturedImage !== "" &&
-          <View style={styles.imageHolder} >
-            <Image source={{ uri: this.state.capturedImage }} style={styles.previewImage} />
-          </View>
-        }
-        <View style={styles.inputBody}>
-        <TouchableHighlight style={styles.appButtonContainer} onPress={this.captureImageButtonHandler}>
-          <Text style={styles.appButtonText}>Capture Image</Text>
-        </TouchableHighlight>
-        </View>
-        
-        <View>
-        <TouchableHighlight style={[styles.appButtonContainer]} onPress={this.registerButtonHandler}>
-          <Text style={styles.appButtonText}>Register</Text>
-        </TouchableHighlight>
-        </View>
-        
-      </View>
-    )
+  if (hasPermission === false) {
+    return <Text style={{ textAlign: "center", marginTop: 100 }}>No access to camera</Text>
   }
+
+  return (
+    <View style={styles.container}>
+      <MaskedView
+        style={StyleSheet.absoluteFill}
+        maskElement={<View style={styles.mask} />}
+      >
+        <Camera
+          style={StyleSheet.absoluteFill}
+          type={Camera.Constants.Type.front}
+          onFacesDetected={onFacesDetected}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.none,
+            runClassifications: FaceDetector.Constants.Classifications.all,
+            minDetectionInterval: 125,
+            tracking: false
+          }}
+          ref={ref => { setCameraRef(ref) }}
+        >
+          <AnimatedCircularProgress
+            style={styles.circularProgress}
+            size={PREVIEW_SIZE}
+            width={5}
+            backgroundWidth={7}
+            fill={state.progressFill}
+            tintColor="#337ab7"
+            backgroundColor="#e8e8e8"
+          />
+        </Camera>
+      </MaskedView>
+      <View style={styles.instructionsContainer}>
+        {!state.processComplete &&
+          <Text style={styles.instructions}>
+            {state.faceDetected === "no" &&
+              state.faceTooBig === "no" &&
+              instructionsText.initialPrompt}
+
+            {state.faceTooBig === "yes" && instructionsText.tooClose}
+
+            {state.faceDetected === "yes" &&
+              state.faceTooBig === "no" &&
+              instructionsText.performActions}
+          </Text>
+        }
+        {state.processComplete &&
+          <Text style={styles.processing}>
+            Registering user...
+          </Text>
+        }
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  inputBody:{
-    padding:20
-  },
   container: {
-    marginTop: 100,
-    alignItems:'center'
+    flex: 1
   },
-  title: {
-    fontSize: 20,
-    color: "#000",
-    textAlign: "center",
-    marginBottom: 15,
-    marginTop: 10
-  },
-  textInput: {
-    textAlign: "center",
-    marginBottom: 7,
-    height: 40,
-    borderWidth: 1,
-    marginTop: 10,
-    borderColor: "#D0D0D0",
-    borderRadius: 5,
-    marginLeft: "5%",
-    width: "90%"
-  },
-  buttonContainer: {
-    height: 45,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 20,
-    borderRadius: 30,
-    marginTop: 20,
-    marginLeft: "5%",
-    width: "90%"
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "bold"
-  },
-  captureButton: {
-    backgroundColor: "#337ab7"
-  },
-  registerButton: {
-    backgroundColor: "#C0C0C0",
-    marginTop: 5
-  },
-  imageHolder: {
-    borderWidth: 1,
-    borderColor: "grey",
-    backgroundColor: "#eee",
-    width: "50%",
-    height: 150,
-    marginTop: 10,
-    marginLeft: 90,
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  previewImage: {
-    width: "100%",
-    height: "100%",
-  },
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: "#009688",
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    width: 200,
-    alignSelf:'center'
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
+  mask: {
+    borderRadius: PREVIEW_SIZE / 2,
+    height: PREVIEW_SIZE,
+    width: PREVIEW_SIZE,
+    marginTop: PREVIEW_RECT.minY,
     alignSelf: "center",
-    textTransform: "uppercase"
+    backgroundColor: "white"
   },
-  input:{
-    width:300,
-    height:45,
-    borderRadius:25,
-    fontSize:16,
-    paddingLeft:45,
-    backgroundColor:'rgba(0,0,0,0.35)',
-    color:'rgba(255,255,255,0.7)',
-    marginHorizontal:25
-},
-inputContainer:{
-    width:"80%",
-  //  backgroundColor:"#465881",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-},
-logo:{
-  width:100,
-  height:100
-}
+  circularProgress: {
+    width: PREVIEW_SIZE,
+    height: PREVIEW_SIZE,
+    marginTop: PREVIEW_RECT.minY,
+    marginLeft: PREVIEW_RECT.minX
+  },
+  instructionsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: PREVIEW_RECT.minY + PREVIEW_SIZE
+  },
+  instructions: {
+    fontSize: 20,
+    textAlign: "center",
+    position: "absolute",
+    top: 15
+  },
+  processing: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+    position: "absolute",
+    top: 25
+  }
 })
